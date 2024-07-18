fa6d97601be9c8e1b0262e0d42e61af88dc0d91a
Bernhard Derix <bderix@web.de>
2024-07-18 11:26:54 +0200

    Lösche Verzeichnis

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,20 +6,6 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit355741a9a6c3a9517978f278046048b7
 {
-    public static $prefixLengthsPsr4 = array (
-        'J' => 
-        array (
-            'JobManager\\' => 11,
-        ),
-    );
-
-    public static $prefixDirsPsr4 = array (
-        'JobManager\\' => 
-        array (
-            0 => __DIR__ . '/../..' . '/',
-        ),
-    );
-
     public static $classMap = array (
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
     );
@@ -27,8 +13,6 @@ class ComposerStaticInit355741a9a6c3a9517978f278046048b7
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit355741a9a6c3a9517978f278046048b7::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit355741a9a6c3a9517978f278046048b7::$prefixDirsPsr4;
             $loader->classMap = ComposerStaticInit355741a9a6c3a9517978f278046048b7::$classMap;
 
         }, null, ClassLoader::class);
